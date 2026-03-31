91676bbe95b236c082c0f725f4d815b5ad74af31
Aaron Meche <aaronmeche18@icloud.com>
2026-03-31 16:49:26 -0500

    reverted to adapter auto, attempting host on firebase

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,14 @@
 import runRue from 'rue-lang';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [runRue()],
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
